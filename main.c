7117a3e62dee9a4b78246bab39d8360aab1e4672
BuzanovNikita <128240165+BuzanovNikita@users.noreply.github.com>
2023-03-18 20:56:10 +0300

    MZ-5

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define LEFT 15
+#define RIGHT 0
+#define FIRSTBIT 1<<15
+#define SHIFTRIGHT >>1
+#define SHIFTYEAR <<9
+#define SHIFTDAY <<5
+
+void PrintBits( unsigned short input ){
+    int i;
+    unsigned short mask=FIRSTBIT;
+    for (i=RIGHT; i<=LEFT; i++){
+        if (mask & input) {printf("1");}
+        else {printf("0");}
+        mask=mask SHIFTRIGHT;
+    }
+    printf("\n");
+}
+
+int main(){
+    unsigned int y, m, d;
+    unsigned short output=0, tmp;
+    scanf("%d %d %d", &y, &m, &d);
+
+    tmp = (unsigned short) y;
+    tmp = tmp SHIFTYEAR;
+    output = output | tmp;
+
+    tmp = (unsigned short) m;
+    tmp = tmp SHIFTDAY;
+    output = output | tmp;
+
+    tmp = (unsigned short) d;
+    output = output | tmp;
+
+    PrintBits( output );
+    printf("%hu\n", output);
+    return 0;
+}
+
